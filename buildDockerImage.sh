10d0d101a82be0f6d350e5edf0e1dc50b282b92d
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-09-07 15:40:25 +0200

    Fix directory with spaces

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -4,7 +4,7 @@ if ! docker stats --no-stream >/dev/null 2>&1; then
     exit 1
 else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-	pushd $DIR > /dev/null
+	pushd "$DIR" > /dev/null
 	VERSION=$(grep --max-count=1 'version' app/main.py | awk -F '"' '{ print $2 }' | awk -F '"' '{ print $1 }')
 	DOCKER_BUILDKIT=1 docker build -f docker/Dockerfile -t internetofus/profile-diversity-manager:$VERSION .
 	popd > /dev/null	
--- startDevelopmentEnvironment.sh
+++ startDevelopmentEnvironment.sh
@@ -3,7 +3,7 @@ if [ -f /.dockerenv ]; then
    echo "You can not start the development environment inside a docker container"
 else
 	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-	pushd $DIR >/dev/null
+	pushd "$DIR" >/dev/null
 	VERSION=$(grep --max-count=1 'version' app/main.py | awk -F '"' '{ print $2 }' | awk -F '"' '{ print $1 }')
 	DOCKER_BUILDKIT=1 docker build -f docker/Dockerfile -t internetofus/profile-diversity-manager:$VERSION .
 	if [ $? -eq 0 ]; then
