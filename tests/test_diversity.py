5cf88d5555f015758c9b9750a81405f1fa446818
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-10-05 16:42:44 +0200

    Test the diversity calculus

--- tests/test_diversity.py
+++ tests/test_diversity.py
@@ -13,3 +13,66 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+import pytest
+from diversity import Agent, Attribute, QN, diversity
+
+
+@pytest.mark.timeout(30)
+def test_calculate_diversity_for_oposed_agents():
+    """Test the diversity over two equals agents"""
+    agents = [
+        Agent("1234", [
+            Attribute("property1", QN, [0, 1], 1),
+            Attribute("property2", QN, [0, 1], 1),
+            Attribute("property3", QN, [0, 1], 1)
+            ]),
+        Agent("abcd", [
+            Attribute("property1", QN, [0, 1], 0),
+            Attribute("property2", QN, [0, 1], 0),
+            Attribute("property3", QN, [0, 1], 0)
+            ])
+    ]
+    reqattr = [Attribute("property1", QN, [0, 1]), Attribute("property2", QN, [0, 1]), Attribute("property3", QN, [0, 1])]
+    div = diversity(agents, reqattr)
+    assert div == 0.5
+
+
+@pytest.mark.timeout(30)
+def test_calculate_diversity_for_equals_agents():
+    """Test the diversity over two equals agents"""
+    agents = [
+        Agent("1234", [
+            Attribute("property1", QN, [0, 1], 1),
+            Attribute("property2", QN, [0, 1], 1),
+            Attribute("property3", QN, [0, 1], 1)
+            ]),
+        Agent("abcd", [
+            Attribute("property1", QN, [0, 1], 1),
+            Attribute("property2", QN, [0, 1], 1),
+            Attribute("property3", QN, [0, 1], 1)
+            ])
+    ]
+    reqattr = [Attribute("property1", QN, [0, 1]), Attribute("property2", QN, [0, 1]), Attribute("property3", QN, [0, 1])]
+    div = diversity(agents, reqattr)
+    assert  div == 0.0
+
+
+@pytest.mark.timeout(30)
+def test_calculate_diversity_for_different_agents():
+    """Test the diversity over two equals agents"""
+    agents = [
+        Agent("1234", [
+            Attribute("property1", QN, [0, 1], 0.1),
+            Attribute("property2", QN, [0, 1], 1),
+            Attribute("property3", QN, [0, 1], 0.91)
+            ]),
+        Agent("abcd", [
+            Attribute("property1", QN, [0, 1], 0.742),
+            Attribute("property2", QN, [0, 1], 0.67),
+            Attribute("property3", QN, [0, 1], 0.28)
+            ])
+    ]
+    reqattr = [Attribute("property1", QN, [0, 1]), Attribute("property2", QN, [0, 1]), Attribute("property3", QN, [0, 1])]
+    div = diversity(agents, reqattr)
+    assert 0.0 <= div <= 1.0
