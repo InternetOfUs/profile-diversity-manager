201a26dd76e6389c65c43749cbb6199f873225e6
Bruno Rosell i Gui <rosell@iiia.csic.es>
2021-10-05 16:42:58 +0200

    Add function to calculate the diversity

--- app/main.py
+++ app/main.py
@@ -21,9 +21,10 @@ import os
 import time
 
 from WNS import sim_str_str, sim_str_str_multiling
+from diversity import Attribute, Agent, QN, diversity
 from fastapi import FastAPI, Body, HTTPException
 from pydantic import BaseModel, Field, HttpUrl
-from typing import List
+from typing import List, Dict
 
 
 # Setup Web services
@@ -47,17 +48,18 @@ class Info(BaseModel):
     vendor: str = Field(...,description="Contain information of the organization that has implemented the API.",example="UDT-IA, IIIA-CSIC")
     license: str = Field(...,description="Contain information of the license of the API.",example="Apache v2")
     
-class StringsSimilarityData(BaseModel):
+class StringsData(BaseModel):
     source: str = Field(...,description="Source string to obtain the similarity.",example="I hate planes")
-    target: str = Field(...,description="Target string to obtain the similarity.",example="I hate helicopters")
-    aggregation: Aggregation = Field(...,description="Statistical function to aggregate the similarity between strings",example="max")
+    
+class UserData(BaseModel):
+    id: str = Field(...,description="Contain the identifier of the user.",example="bgduy367825jg")
+    attributes: Dict[str,float] = Field(...,ge=0.0,le=1.0,description="The attributes of the user, where the key is the name and the value is a number on the range [0,1].")
 
-class StringsSimilarityDataWithLang(StringsSimilarityData):
-    sourceLang: str = Field(...,description="Language of the source string.",example="en")
-    targetLang: str = Field(...,description="Language of the target string.",example="zh")
-
-class SimilarityResult(BaseModel):
-    similarity: float = Field(...,description="The similarity between strings.",example="0.5")
+class UsersData(BaseModel):
+    users:List[UserData] = Field(...,description="A set of users to use.")
+    
+class Diversity(BaseModel):
+    value: float = Field(...,ge=0.0,le=1.0,description="The diversity of a set of users.")
 
 @app.get(
     "/help/info",
@@ -73,5 +75,36 @@ async def get_help_info():
         "softwareVersion": "0.1.0",
         "vendor": "UDT-IA, IIIA-CSIC",
         "license": "Apache v2"
+    } 
+
+@app.post(
+    "/calculateDiversityOf",
+    description="Obtain the diversity that is formed per  a set of users",
+    status_code=200,
+    response_model=Diversity
+)
+async def post_calculate_diversity_of(data:UsersData):
+    
+    agents = []
+    reqattr = []
+    defined_attributes = set()
+    for user in data.users:
+        agent_attributes = []
+        for attribute_id in user.attributes:
+            # Create the user attribute
+            attribute = Attribute(attribute_id,QN,[0,1], user.attributes[attribute_id])
+            agent_attributes.append(attribute)
+            if attribute_id not in defined_attributes:
+                # Add the attribute as defined
+                reqattr.append(Attribute(attribute_id,QN,[0,1]))
+                defined_attributes.add(attribute_id)
+            
+        # Create the agent for the user
+        agents.append(Agent(user.id,agent_attributes))
+        
+    # Calculate the diversity of the agents
+    users_diversity = diversity(agents,reqattr)
+    return {
+        "value": users_diversity
     } 
 
